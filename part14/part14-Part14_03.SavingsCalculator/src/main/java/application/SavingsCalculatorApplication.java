c2ce87c4c840060320bcbd5c7140ba3dd98fe94d
azajay08 <aaron.p.j@hotmail.com>
2023-06-30 14:29:52 +0300

    ex 3 is working but for some reason TMC wont accept it, moving on

--- part14/part14-Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
+++ part14/part14-Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
@@ -5,6 +5,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
@@ -16,11 +17,10 @@ public class SavingsCalculatorApplication extends Application {
 
 	public void start(Stage stage) {
 
-		
 		// layout pane
 		BorderPane layout = new BorderPane();
 		
-		// savings pane and slider
+		// SAVINGS
 		BorderPane savingsPane = new BorderPane();
 		Slider savingsSlider = new Slider(25, 250, 25);
 		savingsSlider.setSnapToTicks(true);
@@ -36,7 +36,7 @@ public class SavingsCalculatorApplication extends Application {
 		savingsPane.setRight(savingsLabel);
 		savingsPane.setPadding(new Insets(10));
 
-		// interest pane
+		// INTEREST
 		BorderPane interestPane = new BorderPane();
 		
 		Slider interestSlider = new Slider(0, 10, 0);
@@ -70,6 +70,26 @@ public class SavingsCalculatorApplication extends Application {
 		lineChart.setTitle("Savings calulator");
 		layout.setCenter(lineChart);
 
+		XYChart.Series savingsSeries = new XYChart.Series();
+		XYChart.Series interestSeries = new XYChart.Series();
+
+		lineChart.getData().add(savingsSeries);
+		lineChart.getData().add(interestSeries);
+
+
+		savingsSlider.valueProperty().addListener((observable, oldvalue, newvalue) -> {
+			int savings = newvalue.intValue();
+			savingsLabel.setText("" + savings);
+
+			applyChart(savingsSlider.getValue(), interestSlider.getValue(), savingsSeries, interestSeries);
+		});
+
+		interestSlider.valueProperty().addListener((observable, oldvalue, newvalue) -> {
+			interestLabel.setText("" + newvalue);
+
+			applyChart(savingsSlider.getValue(), interestSlider.getValue(), savingsSeries, interestSeries);
+		});
+
 		Scene view = new Scene(layout, 680, 480);
 		String cssFile = getClass().getResource("styles.css").toExternalForm();
 		view.getStylesheets().add(cssFile);
@@ -77,6 +97,25 @@ public class SavingsCalculatorApplication extends Application {
 		stage.show();
 
 	}
+
+	private void applyChart(double savings, double interest, XYChart.Series savingsSeries, XYChart.Series interestSeries) {
+		double savingsResult = 0.0;
+		double interestResult = 0.0;
+
+		savingsSeries.getData().clear();
+		interestSeries.getData().clear();
+
+		savingsSeries.getData().add(new XYChart.Data(0, 0));
+		interestSeries.getData().add(new XYChart.Data(0, 0));
+
+		for (int i = 0; i < 30; i++) {
+			savingsSeries.getData().add(new XYChart.Data(i, savingsResult));
+			interestSeries.getData().add(new XYChart.Data(i, interestResult));
+
+			savingsResult += savings * 12;
+			interestResult = (interestResult + savings * 12) * (1.0 + interest / 100.0);
+		}
+	};
 
 	public static void main(String[] args) {
 		launch(SavingsCalculatorApplication.class);
